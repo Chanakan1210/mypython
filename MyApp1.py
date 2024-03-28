06549852c15f2b8770defababa878a8ca93913b7
Chanakan1210 <chanakan121044@gmail.com>
2024-03-28 11:39:42 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -4,3 +4,6 @@ st.title("Website Developing using Python")
 st.header("🍭Website Developing using Python🍭")
 st.subheader("🍬Chanakan Jangeejang🍬")
 st.image('chanakan.jpg')
+
+dt.pd.read_csv('./data/iris.csv')
+st.write(dt.head(10))
