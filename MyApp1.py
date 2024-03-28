23362feaef9de1d5c3fe2807cbafc258ae58ae92
Chanakan1210 <chanakan121044@gmail.com>
2024-03-28 12:09:20 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -17,11 +17,13 @@ st.write(dt.head(10))
 st.subheader("สถิติข้อมูลดอกไม้ Iris")
 st.write('ผลรวม')
 cl1,cl2,cl3,cl4=st.columns(4)
-
 cl1.write(dt['sepal.length'].sum())
 cl2.write(dt['sepal.width'].sum())
 cl3.write(dt['petal.length'].sum())
 cl4.write(dt['petal.width'].sum())
+cols=['sepal.length', 'sepal.width', 'petal.length']
+dx=dt[cols]
+st.bar_chart(dx,x='sepal.length',y='sepal.width',color='petal.length')
 
 st.write('ค่าเฉลี่ย')
 cl1,cl2,cl3,cl4=st.columns(4)
