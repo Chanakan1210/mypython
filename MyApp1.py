cf3f93ccca078d53703d47c14157996e50800960
Chanakan1210 <chanakan121044@gmail.com>
2024-03-28 11:42:01 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
 st.title("Website Developing using Python")
-st.header("🍭Website Developing using Python🍭")
+#st.header("🍭Website Developing using Python🍭")
 st.subheader("🍬Chanakan Jangeejang🍬")
 st.image('chanakan.jpg')
 
-dt.pd.read_csv('./data/iris.csv')
+dt=pd.read_csv('./data/iris.csv')
+st.subheader("🍬ข้อมูลดอกไม้ Iris🍬")
 st.write(dt.head(10))
+
+st.subheader("🍬สถิติข้อมูลดอกไม้ Iris🍬")
