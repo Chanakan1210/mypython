8cf86e3352fc616a44a2db86fd8f2e9b40f88ce8
Chanakan1210 <chanakan121044@gmail.com>
2024-03-28 11:49:57 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -7,11 +7,22 @@ st.subheader("🍬Chanakan Jangeejang🍬")
 st.image('chanakan.jpg')
 
 dt=pd.read_csv('./data/iris.csv')
-st.subheader("🍬ข้อมูลดอกไม้ Iris🍬")
+st.subheader("ข้อมูลดอกไม้ Iris")
 st.write(dt.head(10))
 
-st.subheader("🍬สถิติข้อมูลดอกไม้ Iris🍬")
+#Index(['sepal.length', 'sepal.width', 'petal.length', 'petal.width',
+#       'variety'],
+#      dtype='object')
+
+st.subheader("สถิติข้อมูลดอกไม้ Iris")
 st.write('ผลรวม')
+cl1,cl2,cl3,cl4=st.columns(4)
+
+cl1.write(dt['sepal.length'].sum())
+cl2.write(dt['sepal.width'].sum())
+cl3.write(dt['petal.length'].sum())
+cl4.write(dt['petal.width'].sum())
+
 st.write('ค่าเฉลี่ย')
 st.write('ค่ามากที่สุด')
 st.write('ค่าน้อยที่สุด')
