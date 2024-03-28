b518b5d69543c65444e98c452b48d58e21b25656
Chanakan1210 <chanakan121044@gmail.com>
2024-03-28 11:44:15 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Website Developing using Python")
 #st.header("🍭Website Developing using Python🍭")
@@ -10,3 +11,7 @@ st.subheader("🍬ข้อมูลดอกไม้ Iris🍬")
 st.write(dt.head(10))
 
 st.subheader("🍬สถิติข้อมูลดอกไม้ Iris🍬")
+st.write('ผลรวม')
+st.write('ค่าเฉลี่ย')
+st.write('ค่ามากที่สุด')
+st.write('ค่าน้อยที่สุด')
